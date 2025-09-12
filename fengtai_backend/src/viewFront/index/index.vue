ba219841685c4c78ea678812f7ee9432a4cd4685
张丹鸯 <zhangdanyang_19881020@gmail.com>
2025-09-12 15:16:50 +0800

    index

--- fengtai_backend/src/viewFront/index/index.vue
+++ fengtai_backend/src/viewFront/index/index.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="index-page">
+		<div class="moon"></div>
+		<div class="top-mountain"></div>
+		<div class="bototm-left--house"></div>
 		<div class="bottom-mountain"></div>
 	</div>
 </template>
@@ -12,13 +15,26 @@
 	.index-page {
 		width: 100%;
 		height: 100%;
-		background: url('../../static/f-Images/home-bg.jpg') repeat;
-		background-size: cover;
+		background: url('../../static/f-Images/home-bg.jpg') 100% 100% repeat;
+		background-size: contain;
 		position: relative;
+
+		.moon {
+			position: absolute;
+			background: url('../../static/f-Images/moon.png') 100% 100% no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: right bottom;
+			right: -20px;
+			top: -20px;
+			width: 200px;
+			height: 200px;
+			z-index: 1;
+		}
 
 		.bottom-mountain {
 			position: absolute;
-			background: url('../../static/f-Images/mountain.png') 50% no-repeat;
+			background: url('../../static/f-Images/bottom-house.jpg') 100% 100% no-repeat;
 			background-size: contain;
 			/* 图片等比缩放，完全包含 */
 			background-position: right bottom;
@@ -27,8 +43,36 @@
 			right: 0;
 			width: 100%;
 			height: 100%;
-			z-index: 100;
+			z-index: 1;
+		}
 
+		.bototm-left--house {
+			position: absolute;
+			background: url('../../static/f-Images/bottom-left--house.png') 100% 100% no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: right bottom;
+			/* 背景图居中 */
+			bottom: 0;
+			left: 0;
+			width: 70%;
+			height: 70%;
+			max-width:400px;
+			z-index: 2;
+		}
+
+		.top-mountain {
+			position: absolute;
+			background: url('../../static/f-Images/top-left--bg.png') no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: left top;
+			/* 背景图居中 */
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 1;
 		}
 	}
 </style>
