76881cedd9ba03c386985907af6f6791bdd56d38
张丹鸯 <zhangdanyang_19881020@gmail.com>
2025-09-12 16:50:34 +0800

    灯笼动画

--- fengtai_backend/src/viewFront/index/index.vue
+++ fengtai_backend/src/viewFront/index/index.vue
@@ -6,6 +6,8 @@
 		<div class="bototm-right--house"></div>
 		<div class="bottom-mountain"></div>
 		<div class="bottom-light"></div>
+		<div class="denglong"></div>
+		<div class="logo"></div>
 	</div>
 </template>
 
@@ -20,6 +22,7 @@
 		background: url('../../static/f-Images/home-bg.jpg') 100% 100% repeat;
 		background-size: contain;
 		position: relative;
+		overflow-x: hidden;
 
 		.moon {
 			position: absolute;
@@ -57,12 +60,13 @@
 			/* 背景图居中 */
 			bottom: 0;
 			left: 0;
-			width:100%;
+			width: 100%;
 			height: 100%;
-			max-width:400px;
+			max-width: 400px;
 			z-index: 2;
 		}
-		.bototm-right--house{
+
+		.bototm-right--house {
 			position: absolute;
 			background: url('../../static/f-Images/right-house-2.png') 100% 100% no-repeat;
 			background-size: contain;
@@ -71,9 +75,9 @@
 			/* 背景图居中 */
 			bottom: 0;
 			right: 0;
-			width:100%;
+			width: 100%;
 			height: 100%;
-			max-width:400px;
+			max-width: 400px;
 			z-index: 3;
 		}
 
@@ -90,7 +94,8 @@
 			height: 100%;
 			z-index: 1;
 		}
-		.bottom-light{
+
+		.bottom-light {
 			position: absolute;
 			background: url('../../static/f-Images/bototm-light.png') repeat;
 			background-size: contain;
@@ -102,6 +107,59 @@
 			width: 100%;
 			height: 100%;
 			z-index: 2;
+		}
+
+		.denglong {
+			position: absolute;
+			background: url('../../static/f-Images/denglong-big.png') no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: left top;
+			/* 背景图居中 */
+			top: 70px;
+			left: 30%;
+			width: 50%;
+			height: 50%;
+			z-index: 10;
+			animation: moveAndDisappear 15s ease-in-out infinite;
+		}
+
+		.logo {
+			position: absolute;
+			background: url('../../static/f-Images/logo-together.png') no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: center top;
+			/* 背景图居中 */
+			top: 200px;
+			left: 60px;
+			width: calc(100% - 120px);
+			height: 100%;
+			z-index: 11;
+		}
+	}
+
+
+	@keyframes moveAndDisappear {
+		0% {
+		    transform: translateY(0) rotate(0deg);
+		    opacity: 1;
+		}
+		// 25% {
+		//     transform: translateY(-100px) translateX(20px) rotate(5deg);
+		//     opacity: 0.9;
+		// }
+		// 50% {
+		//     transform: translateY(-200px) translateX(-15px) rotate(-3deg);
+		//     opacity: 0.8;
+		// }
+		// 75% {
+		//     transform: translateY(-300px) translateX(100px) rotate(2deg);
+		//     opacity: 0.5;
+		// }
+		100% {
+		    transform: translateY(-400px) translateX(300px) rotate(0deg);
+		    opacity: 0;
 		}
 	}
 </style>
