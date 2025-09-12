3084f2e0770a39d87a20556f538e941e55eacfac
张丹鸯 <zhangdanyang_19881020@gmail.com>
2025-09-12 15:57:54 +0800

    bg

--- fengtai_backend/src/viewFront/index/index.vue
+++ fengtai_backend/src/viewFront/index/index.vue
@@ -3,7 +3,9 @@
 		<div class="moon"></div>
 		<div class="top-mountain"></div>
 		<div class="bototm-left--house"></div>
+		<div class="bototm-right--house"></div>
 		<div class="bottom-mountain"></div>
+		<div class="bottom-light"></div>
 	</div>
 </template>
 
@@ -29,12 +31,12 @@
 			top: -20px;
 			width: 200px;
 			height: 200px;
-			z-index: 1;
+			z-index: 3;
 		}
 
 		.bottom-mountain {
 			position: absolute;
-			background: url('../../static/f-Images/bottom-house.jpg') 100% 100% no-repeat;
+			background: url('../../static/f-Images/bottom-half-shan.jpg') 100% 100% no-repeat;
 			background-size: contain;
 			/* 图片等比缩放，完全包含 */
 			background-position: right bottom;
@@ -48,22 +50,36 @@
 
 		.bototm-left--house {
 			position: absolute;
-			background: url('../../static/f-Images/bottom-left--house.png') 100% 100% no-repeat;
+			background: url('../../static/f-Images/bottom-left--cloud.png') 100% 100% no-repeat;
 			background-size: contain;
 			/* 图片等比缩放，完全包含 */
 			background-position: right bottom;
 			/* 背景图居中 */
 			bottom: 0;
 			left: 0;
-			width: 70%;
-			height: 70%;
+			width:100%;
+			height: 100%;
 			max-width:400px;
 			z-index: 2;
+		}
+		.bototm-right--house{
+			position: absolute;
+			background: url('../../static/f-Images/right-house-2.png') 100% 100% no-repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: right bottom;
+			/* 背景图居中 */
+			bottom: 0;
+			right: 0;
+			width:100%;
+			height: 100%;
+			max-width:400px;
+			z-index: 3;
 		}
 
 		.top-mountain {
 			position: absolute;
-			background: url('../../static/f-Images/top-left--bg.png') no-repeat;
+			background: url('../../static/f-Images/top-left--sky2.jpg') no-repeat;
 			background-size: contain;
 			/* 图片等比缩放，完全包含 */
 			background-position: left top;
@@ -73,6 +89,19 @@
 			width: 100%;
 			height: 100%;
 			z-index: 1;
+		}
+		.bottom-light{
+			position: absolute;
+			background: url('../../static/f-Images/bototm-light.png') repeat;
+			background-size: contain;
+			/* 图片等比缩放，完全包含 */
+			background-position: left top;
+			/* 背景图居中 */
+			bottom: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			z-index: 2;
 		}
 	}
 </style>
