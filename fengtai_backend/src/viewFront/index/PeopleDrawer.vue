20fdd05bb812dc33d4928a72eb60d00def2e2421
张丹鸯 <zhangdanyang_19881020@gmail.com>
2025-09-13 12:55:25 +0800

    选择人

--- fengtai_backend/src/viewFront/index/PeopleDrawer.vue
+++ fengtai_backend/src/viewFront/index/PeopleDrawer.vue
@@ -17,6 +17,7 @@
 					<el-col :span="14" class="relative-info">
 						<div class="name-tr">
 							<label>{{ relative.name }}</label>
+
 							<el-tag type="danger" v-if="relative.gender=='female'">女</el-tag>
 							<el-tag type="primary" v-else-if="relative.gender=='male'">男</el-tag>
 							<el-tag type="info" v-else>--</el-tag>
@@ -31,7 +32,7 @@
 
 			<!-- 重新查询按钮 -->
 			<div class="footer">
-				<div class="search-btn" @click="handleRefresh">重新查询</div>
+				<div class="search-btn" @click="choosePeople">选择</div>
 			</div>
 		</div>
 	</el-dialog>
@@ -63,11 +64,11 @@
 	// 亲戚数据
 	const relatives = ref([]);
 	const relativeCount = ref(0);
-	
-	const scrollHeight=computed(()=>{
+
+	const scrollHeight = computed(() => {
 		const windowHeight = document.documentElement.clientHeight;
 		console.log(windowHeight); // 输出浏览器可视区域的高度（以像素为单位）
-        return windowHeight-400;
+		return windowHeight - 400;
 	})
 
 	// 显示的亲戚数量
@@ -88,9 +89,9 @@
 	};
 
 	// 刷新查询
-	const handleRefresh = () => {
+	const choosePeople = () => {
 		// 在这里添加重新查询的逻辑，刷新亲戚数据
-		ElMessage.success("重新查询中...");
+		ElMessage.success("选中...");
 	};
 
 	const open = (searchedPeopleList) => {
@@ -108,19 +109,30 @@
 
 <style lang="scss" scoped>
 	.custom-dialog {
-		.search-btn{
-			margin:0 auto;
+		.search-btn {
+			margin: 0 auto;
 		}
-		.relative-list{
-			
+
+		.relative-list {
+			.relative-item {
+				margin-bottom: 12px;
+				background: #fff;
+				border-radius: 8px;
+				padding: 10px;
+				box-shadow: 0 1px 0 rgba(139, 69, 19, 0.2);
+
+				&:hover {
+					background: 0 rgba(139, 69, 19, 0.1);
+				}
+			}
 		}
-		
-		
+
+
 	}
 
 	.name-tr {
 		display: flex;
-		margin-bottom:3px;
+		margin-bottom: 3px;
 
 		label {
 			font-size: 15px;
@@ -142,7 +154,7 @@
 
 	.header-info {
 		font-size: 16px;
-		font-size:20px;
+		font-size: 20px;
 		text-align: center;
 		color: #3d3d3d;
 		transform: translateY(-25px);
@@ -152,13 +164,7 @@
 		color: #ff7f50;
 	}
 
-	.relative-item {
-		margin-bottom: 12px;
-		background:#fff;
-		border-radius: 8px;
-		padding:10px;
-		box-shadow: 0 1px 0 rgba(139,69,19,0.2);
-	}
+
 
 	.relative-avatar {
 		width: 50px;
